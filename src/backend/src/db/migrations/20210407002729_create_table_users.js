bfb0a12dcb06b6d75698e6cfdb74f6f6338edc2c
Isac Moura <contatoisacmoura@outlook.com>
2021-04-06 21:46:50 -0300

    Creating user model

---
+++ src/backend/src/db/migrations/20210407002729_create_table_users.js
@@ -0,0 +1,13 @@
+
+exports.up = function(knex) {
+    return knex.schema.createTable('user', function(table) {
+        table.increments('id').primary();
+        table.string('name').notNullable();
+        table.string('email').unique().notNullable();
+        table.foreign('medication_id').references('id').inTable('medication');
+    });
+};
+
+exports.down = function(knex) {
+    return knex.schema.dropTable('user');
+};
