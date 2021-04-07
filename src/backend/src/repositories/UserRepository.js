525a74b74a5eddbffa21b7ce0db46c86ab88ed04
Isac Moura <contatoisacmoura@outlook.com>
2021-04-07 19:13:42 -0300

    User repository implementation

--- src/backend/src/repositories/UserRepository.js
+++ src/backend/src/repositories/UserRepository.js
@@ -3,7 +3,7 @@ const connection = require('../db/index');
 module.exports = {
     async getAllUserInformation(request, response) {
         try {
-            const users = await connection()
+            const users = await connection('user_medication AS um')
             .select(
                 ['u.name',
                 'u.email',
@@ -13,10 +13,9 @@ module.exports = {
                 'a.description AS app_description',
                 'a.date']
                 )
-            .from('user_medication AS um',)
-            .innerJoin('user AS u', 'um.user_id', '=', 'u.id')
+            .join('user AS u', 'um.user_id', '=', 'u.id')
             .join('medication AS m', 'm.id', '=', 'um.medication_id')
-            .innerJoin('user_appointment AS ua', 'ua.user_id', '=', 'u.id')
+            .join('user_appointment AS ua', 'ua.user_id', '=', 'u.id')
             .join('appointment AS a', 'a.id', '=', 'ua.appointment_id');
 
             return response.json(users);
@@ -24,4 +23,30 @@ module.exports = {
             return response.json(`The following error occurred: ${error.message}`);
         }
     },
+
+    async getUserInformation(request, response) {
+        try {
+            const user_id = request.params.user_id;
+
+            const user = await connection('user_medication AS um')
+            .select(
+                ['u.name',
+                'u.email',
+                'm.name AS med_name',
+                'm.description AS med_description',
+                'a.name AS app_name',
+                'a.description AS app_description',
+                'a.date']
+                )
+            .join('user AS u', 'um.user_id', '=', 'u.id')
+            .join('medication AS m', 'm.id', '=', 'um.medication_id')
+            .join('user_appointment AS ua', 'ua.user_id', '=', 'u.id')
+            .join('appointment AS a', 'a.id', '=', 'ua.appointment_id')
+            .where('u.id', user_id);
+
+            return response.json(user);
+        } catch (error) {
+            return response.json(`The following error occurred: ${error.message}`);
+        }
+    }
 }
--- src/backend/src/routes.js
+++ src/backend/src/routes.js
@@ -6,5 +6,6 @@ const UserRepository = require('./repositories/UserRepository');
 const routes = express.Router();
 
 routes.get('/', UserRepository.getAllUserInformation);
+routes.get('/user/:user_id', UserRepository.getUserInformation);
 
 module.exports = routes;
