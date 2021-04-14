87a21f5363c377fe15d94b4e8b0f04b7c23da747
Isac Moura <contatoisacmoura@outlook.com>
2021-04-14 14:58:31 -0300

    Removing unused imports

--- src/mobile/my_clinical_summary/lib/main.dart
+++ src/mobile/my_clinical_summary/lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:my_clinical_summary/AppModule.dart';
 import 'package:my_clinical_summary/resources/consts/AppStrings.dart';
-import 'package:timeago/timeago.dart' as timeAgo;
 
 Future main() async {
   await startMain();
@@ -10,6 +9,5 @@ Future main() async {
 
 Future startMain() async {
   WidgetsFlutterBinding.ensureInitialized();
-  timeAgo.setLocaleMessages(AppStrings.defaultLocale, timeAgo.PtBrMessages());
   runApp(ModularApp(module: AppModule()));
 }
